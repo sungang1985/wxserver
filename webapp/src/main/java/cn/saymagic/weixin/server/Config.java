e8f4970c308ec3c7bec0b49c8cc5b4f2419c1a5c
sungang <oxpatient1985@gmail.com>
2015-09-15 23:59:40 +0800

    Update Config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "sunjiahe20141025";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "d81dded8eb3cfe5efa2af800819f54ed"; 
 	
 }
